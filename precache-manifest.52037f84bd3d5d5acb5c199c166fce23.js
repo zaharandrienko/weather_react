24dda12103a8192418e680de5f7ea150edbe67d6
Zahar Andrienko <zaharandrienko88@gmail.com>
2018-12-08 18:59:12 +0200

    Updates

---
+++ precache-manifest.52037f84bd3d5d5acb5c199c166fce23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a8f2e55a9f8c564d588",
+    "url": "/weather_react/static/js/runtime~main.9a8f2e55.js"
+  },
+  {
+    "revision": "449c4b71b8ef05d099e7",
+    "url": "/weather_react/static/js/main.449c4b71.chunk.js"
+  },
+  {
+    "revision": "066d5c9fa1bdcc838e7f",
+    "url": "/weather_react/static/js/1.066d5c9f.chunk.js"
+  },
+  {
+    "revision": "449c4b71b8ef05d099e7",
+    "url": "/weather_react/static/css/main.68f001a0.chunk.css"
+  },
+  {
+    "revision": "3d7adbb9f34c96b80fa2880793f8ce5a",
+    "url": "/weather_react/index.html"
+  }
+];
